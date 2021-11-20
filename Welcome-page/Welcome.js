87a4eb435a72e76ff4ef83d6ea5eaa668482361f
Jim911122 <matej.kucera.mail@gmail.com>
2021-11-20 17:36:11 +0100

    video & buttons

--- Welcome-page/Welcome.js
+++ Welcome-page/Welcome.js
@@ -1,6 +1,8 @@
+// color picker js//
 const colorPicker = document.getElementById("colorpicker");
 const body = document.querySelector("body");
 
+// scan color picker//
 setInterval(function () {
   body.style.color = colorPicker.value;
 }, 1000);
